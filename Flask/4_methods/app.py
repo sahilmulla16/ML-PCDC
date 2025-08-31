9f15a6faf1d0d53c16697cb23b837371c555905c
sahilmulla16 <sahilmulla9152@gmail.com>
2025-08-31 14:33:34 +0530

    4_methods update

--- Flask/4_methods/app.py
+++ Flask/4_methods/app.py
@@ -1,12 +1,13 @@
-from flask import Flask , render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/' , methods  = ['GET','POST'])
-def basic( ):
+@app.route ('/', methods=['GET', 'POST'])
+def basic ():
     if request.method == 'POST':
         text = request.form['firstname']
-    return render_template('index.html' name=text)
+        return render_template ('index.html', name=text)
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run (debug = True)
+    app.run (debug=True)
